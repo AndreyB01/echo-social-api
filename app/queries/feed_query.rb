83ad3eebedc99e87cc0e0b789cf992682c65f7eb
AndreyB01 <baltsevichandrey2001@gmail.com>
2026-05-22 15:20:48 +0300

    Improve feed query pagination

--- app/queries/feed_query.rb
+++ app/queries/feed_query.rb
@@ -1,6 +1,10 @@
 class FeedQuery
-  def initialize(user:)
+  DEFAULT_PER_PAGE = 20
+
+  def initialize(user:, page: 1, per_page: DEFAULT_PER_PAGE)
     @user = user
+    @page = page
+    @per_page = per_page
   end
 
   def call
@@ -8,15 +12,15 @@ class FeedQuery
       .includes(:user, :hashtags)
       .where(user_id: feed_user_ids)
       .order(created_at: :desc)
+      .page(page)
+      .per(per_page)
   end
 
   private
 
-  attr_reader :user
+  attr_reader :user, :page, :per_page
 
   def feed_user_ids
-    user
-      .following
-      .pluck(:id) + [user.id]
+    user.following.pluck(:id) + [user.id]
   end
 end
