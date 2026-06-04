038c721d2b698d5409f2b688d7adf4a72afda00f
AndreyB01 <baltsevichandrey2001@gmail.com>
2026-06-04 11:05:58 +0300

    fix: adapt tests and email confirmations for RS256 and global EmailConfirmationService

--- spec/channels/application_cable/connection_spec.rb
+++ spec/channels/application_cable/connection_spec.rb
@@ -2,21 +2,19 @@ require "rails_helper"
 
 RSpec.describe ApplicationCable::Connection, type: :channel do
   let!(:user) { create(:user) }
+  let!(:session) { create(:user_session, user: user) }
 
   let(:token) do
-    Jwt::Encoder.call(user_id: user.id)
+    Jwt::AccessTokenEncoder.call(user: user, session: session)
   end
 
   it "connects with valid token" do
     connect "/cable?token=#{token}"
 
-    expect(connection.current_user)
-      .to eq(user)
+    expect(connection.current_user).to eq(user)
   end
 
   it "rejects connection without token" do
-    expect {
-      connect "/cable"
-    }.to have_rejected_connection
+    expect { connect "/cable" }.to have_rejected_connection
   end
 end
