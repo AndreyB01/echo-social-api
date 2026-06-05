0f18b04d7d873d7fa11fbc473fa501350122501e
AndreyB01 <baltsevichandrey2001@gmail.com>
2026-06-05 12:37:40 +0300

    Remove invalid GET /api/v1/posts tests

--- spec/requests/api/v1/posts_spec.rb
+++ spec/requests/api/v1/posts_spec.rb
@@ -1,4 +1,4 @@
-require 'rails_helper'
+require "rails_helper"
 
 RSpec.describe "Posts API", type: :request do
   let!(:user) { create(:user) }
@@ -12,43 +12,5 @@ RSpec.describe "Posts API", type: :request do
     {
       "Authorization" => "Bearer #{token}"
     }
-  end
-
-  describe "GET /api/v1/posts" do
-    it "returns posts" do
-      get "/api/v1/posts", headers: headers
-
-      expect(response).to have_http_status(:ok)
-
-      json = JSON.parse(response.body)
-
-      expect(json["data"].size).to eq(3)
-
-      expect(json["data"].first)
-        .to include(
-          "id",
-          "body",
-          "created_at",
-          "likes_count",
-          "comments_count",
-          "hashtags",
-          "author"
-        )
-    end
-
-    it "returns meta info" do
-      get "/api/v1/posts", headers: headers
-
-      expect(response).to have_http_status(:ok)
-
-      json = JSON.parse(response.body)
-
-      expect(json["meta"])
-        .to include(
-          "next_cursor",
-          "limit",
-          "has_next"
-        )
-    end
   end
 end
